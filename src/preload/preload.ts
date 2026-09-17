be04b8db86a1ea4731d3c9c4bfe83d01d4ec4325
Juls <juliana.penafiel@campusucc.edu.co>
2026-09-17 17:41:11 -0500

    Expose planner api via contextbridge in preload

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -6,6 +6,31 @@ import {
   type SystemCapabilities,
   type SystemStatusData
 } from "../shared/contracts";
+import type {
+  CategoryListInput,
+  CategoryRecord,
+  CompleteTaskInput,
+  CreateCategoryInput,
+  CreateEventInput,
+  CreateReminderInput,
+  CreateTaskInput,
+  EventListInput,
+  EventRecord,
+  GetTodayScheduleInput,
+  GetWeekScheduleInput,
+  PlannerListData,
+  PlannerMutationData,
+  PlannerOperationResult,
+  ReminderListInput,
+  ReminderRecord,
+  TaskListInput,
+  TaskRecord,
+  TodayScheduleData,
+  UpdateCategoryInput,
+  UpdateEventInput,
+  UpdateTaskInput,
+  WeekScheduleData
+} from "../shared/planner-contracts";
 
 const aresApi = {
   system: {
@@ -17,6 +42,74 @@ const aresApi = {
       ipcRenderer.invoke(IPC_CHANNELS.system.getCapabilities) as Promise<
         OperationResult<SystemCapabilities>
       >
+  },
+  planner: {
+    categories: {
+      create: (input: CreateCategoryInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.categories.create, input) as Promise<
+          PlannerOperationResult<PlannerMutationData<CategoryRecord>>
+        >,
+      list: (input: CategoryListInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.categories.list, input) as Promise<
+          PlannerOperationResult<PlannerListData<CategoryRecord>>
+        >,
+      update: (input: UpdateCategoryInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.categories.update, input) as Promise<
+          PlannerOperationResult<PlannerMutationData<CategoryRecord>>
+        >
+    },
+    tasks: {
+      create: (input: CreateTaskInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.tasks.create, input) as Promise<
+          PlannerOperationResult<PlannerMutationData<TaskRecord>>
+        >,
+      list: (input: TaskListInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.tasks.list, input) as Promise<
+          PlannerOperationResult<PlannerListData<TaskRecord>>
+        >,
+      update: (input: UpdateTaskInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.tasks.update, input) as Promise<
+          PlannerOperationResult<PlannerMutationData<TaskRecord>>
+        >,
+      complete: (input: CompleteTaskInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.tasks.complete, input) as Promise<
+          PlannerOperationResult<PlannerMutationData<TaskRecord>>
+        >
+    },
+    events: {
+      create: (input: CreateEventInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.events.create, input) as Promise<
+          PlannerOperationResult<PlannerMutationData<EventRecord>>
+        >,
+      list: (input: EventListInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.events.list, input) as Promise<
+          PlannerOperationResult<PlannerListData<EventRecord>>
+        >,
+      update: (input: UpdateEventInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.events.update, input) as Promise<
+          PlannerOperationResult<PlannerMutationData<EventRecord>>
+        >
+    },
+    reminders: {
+      create: (input: CreateReminderInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.reminders.create, input) as Promise<
+          PlannerOperationResult<PlannerMutationData<ReminderRecord>>
+        >,
+      list: (input: ReminderListInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.reminders.list, input) as Promise<
+          PlannerOperationResult<PlannerListData<ReminderRecord>>
+        >
+    },
+    schedule: {
+      getToday: (input: GetTodayScheduleInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.schedule.getToday, input) as Promise<
+          PlannerOperationResult<TodayScheduleData>
+        >,
+      getWeek: (input: GetWeekScheduleInput) =>
+        ipcRenderer.invoke(IPC_CHANNELS.planner.schedule.getWeek, input) as Promise<
+          PlannerOperationResult<WeekScheduleData>
+        >
+    }
   }
 } satisfies AresApi;
 
