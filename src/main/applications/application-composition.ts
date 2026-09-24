2c198416ce62ce7a6472fcad97b5a59970a7b6d9
Juls <juliana.penafiel@campusucc.edu.co>
2026-09-24 10:28:52 -0500

    Compose application services including chrome registration

--- src/main/applications/application-composition.ts
+++ src/main/applications/application-composition.ts
@@ -1,8 +1,20 @@
 import { createApplicationService, type ApplicationService } from "./application-service";
+import {
+  createChromeRegistrationService,
+  type ChromeRegistrationService
+} from "./chrome-registration-service";
 
 let applicationService: ApplicationService | undefined;
+let chromeRegistrationService: ChromeRegistrationService | undefined;
 
 export const getApplicationService = (): ApplicationService => {
   applicationService ??= createApplicationService();
   return applicationService;
+};
+
+export const getChromeRegistrationService = (): ChromeRegistrationService => {
+  chromeRegistrationService ??= createChromeRegistrationService({
+    applicationService: getApplicationService()
+  });
+  return chromeRegistrationService;
 };
